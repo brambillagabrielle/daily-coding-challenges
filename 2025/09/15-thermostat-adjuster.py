70b64184c74443412133703a87ea4ba1b561c5fc
brambillagabrielle <gabriellebrambillacc@gmail.com>
2025-09-15 11:07:12 -0300

    2025-09-15

--- 2025/09/15-thermostat-adjuster.py
+++ 2025/09/15-thermostat-adjuster.py
@@ -4,3 +4,22 @@ Given the current temperature of a room and a target temperature, return a strin
  - Return "cool" if the current temperature is above the target.
  - Return "hold" if the current temperature is equal to the target.
 '''
+
+def adjust_thermostat(temp, target):
+    indication = ""
+
+    if temp < target:
+        indication = "heat"
+    elif temp > target:
+        indication = "cool"
+    else:
+        indication = "hold"
+
+    return indication
+
+print("Success") if adjust_thermostat(68, 72) == "heat" else print("Failed")
+print("Success") if adjust_thermostat(75, 72) == "cool" else print("Failed")
+print("Success") if adjust_thermostat(72, 72) == "hold" else print("Failed")
+print("Success") if adjust_thermostat(-20.5, -10.1) == "heat" else print("Failed")
+print("Success") if adjust_thermostat(100, 99.9) == "cool" else print("Failed")
+print("Success") if adjust_thermostat(0.0, 0.0) == "hold" else print("Failed")
